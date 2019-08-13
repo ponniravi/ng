67fc5252bc0d4d17db6dc07fa0c45f00e126ab13
ponniravi <53963088+ponniravi@users.noreply.github.com>
2019-08-13 15:18:09 +0530

    Create 13.py

---
+++ 13.py
@@ -0,0 +1,8 @@
+t,q=map(int,input().split())
+c=0
+l=list(map(int,input().split()))
+for i in range(len(l)-1):
+    for j in range(i+1,len(l)):
+        if abs(l[i]-l[j])==q:
+            c=c+1
+print(c) 
